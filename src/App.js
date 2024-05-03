df23743bce659c988d7097da9a474de69a178a3a
pm-2001 <princemauryapm2001@gmail.com>
2024-05-03 23:20:13 +0530

    Added basic navbar

--- src/App.js
+++ src/App.js
@@ -1,114 +1,21 @@
 import logo from './logo.svg';
 import './App.css';
+import CustomNav from './components/navbar/CustomNav.jsx';
 
 function App() {
-  let data = [
-    {
-    "name": "John Doe",
-    "Purpose": "john@gmail.com",
-    "Functionality": "123456",
-    "Quantity Ordered": "123456",
-    "Quantity Possessed": "123456",
-    "Current Location": "123456",
-    "Remarks": "123456",
-    "Club Tenure": "123456",
-    "Ordered Date": "123456",
-    "Link to Product": "123456",
-    "Invoice Folder Link": "123456",
-    "Total Price": "123456"
-  },
-  
-    {
-    "name": "John Doe",
-    "Purpose": "john@gmail.com",
-    "Functionality": "123456",
-    "Quantity Ordered": "123456",
-    "Quantity Possessed": "123456",
-    "Current Location": "123456",
-    "Remarks": "123456",
-    "Club Tenure": "123456",
-    "Ordered Date": "123456",
-    "Link to Product": "123456",
-    "Invoice Folder Link": "123456",
-    "Total Price": "123456"
-  },
-    {
-    "name": "John Doe",
-    "Purpose": "john@gmail.com",
-    "Functionality": "123456",
-    "Quantity Ordered": "123456",
-    "Quantity Possessed": "123456",
-    "Current Location": "123456",
-    "Remarks": "123456",
-    "Club Tenure": "123456",
-    "Ordered Date": "123456",
-    "Link to Product": "123456",
-    "Invoice Folder Link": "123456",
-    "Total Price": "123456"
-  },
-  
-    {
-    "name": "John Doe",
-    "Purpose": "john@gmail.com",
-    "Functionality": "123456",
-    "Quantity Ordered": "123456",
-    "Quantity Possessed": "123456",
-    "Current Location": "123456",
-    "Remarks": "123456",
-    "Club Tenure": "123456",
-    "Ordered Date": "123456",
-    "Link to Product": "123456",
-    "Invoice Folder Link": "123456",
-    "Total Price": "123456"
-  },
-
-
-]
 
 
   return (
     <div className="App">
-      <div>Inventory Management</div>
-
-  <table>
-    
-  <tr class="table-row">
-    <th>Name</th>
-    <th>Purpose</th>
-    <th>Functionality</th>
-    <th>Quantity Ordered</th>
-    <th>Quantity Possessed</th>
-    <th>Current Location</th>
-    <th>Remarks</th>
-    <th>Club Tenure</th>
-    <th>Ordered Date</th>
-    <th>Link to Product</th>
-    <th>Invoice Folder Link</th>
-    <th>Total Price</th>
-  </tr>
-
-  {
-        data.map((item, index) => {
-          return (
-            <tr key={index} class="table-row">
-              <td>{item.name}</td>
-              <td>{item.Purpose}</td>
-              <td>{item.Functionality}</td>
-              <td>{item['Quantity Ordered']}</td>
-              <td>{item['Quantity Possessed']}</td>
-              <td>{item['Current Location']}</td>
-              <td>{item.Remarks}</td>
-              <td>{item['Club Tenure']}</td>
-              <td>{item['Ordered Date']}</td>
-              <td>{item['Link to Product']}</td>
-              <td>{item['Invoice Folder Link']}</td>
-              <td>{item['Total Price']}</td>
-            </tr>
-          )
-        })
-      }
-
-</table>
+      <CustomNav
+      li={[
+        ["Dashboard", "img/dashboard.svg"],
+        ["Restautant’s", "img/restaurant.svg"],
+        ["Manage User’s", "img/manage user.svg"],
+        ["Manage Order’s", "img/manage  order.svg"],
+        ["Manage Coupon’s", "img/manage coupon.svg"]
+      ]}
+    />
     </div>
   );
 }
