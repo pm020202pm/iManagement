583a976125a2037692bc83d3c3ad7899f79754d9
pm-2001 <princemauryapm2001@gmail.com>
2024-05-04 00:40:55 +0530

    improved navbar

--- src/App.js
+++ src/App.js
@@ -7,15 +7,7 @@ function App() {
 
   return (
     <div className="App">
-      <CustomNav
-      li={[
-        ["Dashboard", "img/dashboard.svg"],
-        ["Restautant’s", "img/restaurant.svg"],
-        ["Manage User’s", "img/manage user.svg"],
-        ["Manage Order’s", "img/manage  order.svg"],
-        ["Manage Coupon’s", "img/manage coupon.svg"]
-      ]}
-    />
+      <CustomNav/>
     </div>
   );
 }
--- src/components/navbar/CustomNav.jsx
+++ src/components/navbar/CustomNav.jsx
@@ -1,39 +1,48 @@
-import React, { useState } from "react";
-import "./_CustomNav.css";
-const CustomNav = ({ li }) => {
-  const [window, setWindow] = useState(false);
+import * as React from 'react';
+import Box from '@mui/material/Box';
+import Drawer from '@mui/material/Drawer';
+import Button from '@mui/material/Button';
+import List from '@mui/material/List';
+import ListItem from '@mui/material/ListItem';
+import ListItemButton from '@mui/material/ListItemButton';
+import ListItemIcon from '@mui/material/ListItemIcon';
+import ListItemText from '@mui/material/ListItemText';
+import InboxIcon from '@mui/icons-material/MoveToInbox';
+import MailIcon from '@mui/icons-material/Mail';
+import CalendarViewDayIcon from '@mui/icons-material/CalendarViewDay';
 
-  let openClose = () => {
-    if (window === false) {
-      setWindow(true);
-    } else {
-      setWindow(false);
-    }
+export default function CustomNav() {
+  const [open, setOpen] = React.useState(false);
+
+  const toggleDrawer = (newOpen) => () => {
+    setOpen(newOpen);
   };
-  return (
-    <nav className="navbar-menu" style={{ width: window === false ? 250 : 60 }}>
-      <div className="burger" onClick={() => openClose()}>
-        <img src="img/menu.svg" alt="burger" />
-      </div>
-      <ul className="navbar__list">
-        {li.map((item, i) => (
-          <div className="navbar__li-box" key={i}>
-            <img
-              src={item[1]}
-              alt={item[1]}
-              style={{ paddingLeft: window === false ? 27 : 17 }}
-            />
-            <li
-              className="navbar__li"
-              style={{ display: window === false ? "inline-block" : "none" }}
-            >
-              {item[0]}
-            </li>
-          </div>
-        ))}
-      </ul>
-    </nav>
-  );
-};
 
-export default CustomNav;
+  const DrawerList = (
+    <Box sx={{ width: 200 }} role="presentation" onClick={toggleDrawer(false)}>
+      <List>
+        {['Home', 'Inventory', 'Feedback', 'About'].map((text, index) => (
+          <ListItem key={text} disablePadding>
+            <ListItemButton>
+              <ListItemIcon>
+                {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
+              </ListItemIcon>
+              <ListItemText primary={text} />
+            </ListItemButton>
+          </ListItem>
+        ))}
+      </List>
+    </Box>
+  );
+
+  return (
+    <div style={{ display: 'flex', justifyContent: 'flex-start', alignItems: 'flex-start' }}>
+      <Button onClick={toggleDrawer(true)}>
+        <CalendarViewDayIcon />
+      </Button>
+      <Drawer open={open} onClose={toggleDrawer(false)}>
+        {DrawerList}
+      </Drawer>
+    </div>
+  );
+}
